06a3bfe81068e9d6737bbb9e2bc6b4db62cb9dfc
tenoriodouglas <tenoriodouglas@live.com>
2021-04-20 21:30:55 -0300

    remove proxy stuff

--- bot.py
+++ bot.py
@@ -9,16 +9,7 @@ from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 from unidecode import unidecode
 
-#meu_id=424534967
-#karinne_id=894335109
 
-# You can leave this bit out if you're using a paid PythonAnywhere account
-proxy_url = "http://proxy.server:3128"
-telepot.api._pools = {
-    'default': urllib3.ProxyManager(proxy_url=proxy_url, num_pools=3, maxsize=10, retries=False, timeout=30),
-}
-telepot.api._onetime_pool_spec = (urllib3.ProxyManager, dict(proxy_url=proxy_url, num_pools=1, maxsize=1, retries=False, timeout=30))
-# end of the stuff that's only needed for free accounts
 
 bot = telepot.Bot('908840680:AAFIPWgGnOvkLaOmNNdwkYeLnTyMKQ8eizs')
 
